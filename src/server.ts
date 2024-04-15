aa3d1be5c5ace97ed0de799182142eac8004a7fe
m-la-ra <martin.lars.rahm+msd@gmail.com>
2024-04-16 00:57:19 +0200

    Project setup - lowdb testing

--- src/server.ts
+++ src/server.ts
@@ -13,11 +13,23 @@ const BASE_API_URL = "https://api.coingecko.com/api/v3";
 const API_PARAMS =
   "/simple/price?ids=bitcoin&vs_currencies=czk,eur&x_cg_demo_api_key=";
 
+// const endpoints = [
+//   "https://api.coindesk.com/v1/bpi/currentprice/czk.json",
+//   "https://api.coindesk.com/v1/bpi/currentprice/eur.json",
+// ];
+
+// Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
+//   ([{ data: bpi }, { data: time }]) => {
+//     console.log({ bpi, time });
+//   }
+// );
+
 app.get("/price", async (req, res) => {
   try {
     const response = await axios.get<BitcoinPriceResponse>(
       BASE_API_URL + API_PARAMS + API_KEY
     );
+
     const data = response.data.bitcoin;
     const requestTime = response.headers.date;
 
@@ -40,31 +52,70 @@ app.get("/price", async (req, res) => {
     });
 
     const currentDate = new Date().toISOString().slice(0, 10);
-    const dailyAverageCZK = db
-      .get("bitcoinPrices")
-      .filter({ currency: "CZK", createdAt: currentDate })
-      .map("rate")
-      .uniq()
-      .mean()
-      .value();
+    const currentMonth = currentDate.slice(0, 7);
+    console.log(currentMonth);
+    console.log(currentDate);
+
+    const calculateAveragePrice = (currency: string, createdAt: string) => {
+      return db
+        .get("bitcoinPrices")
+        .filter({ currency: currency, createdAt: createdAt })
+        .map("rate")
+        .uniq()
+        .mean()
+        .value();
+    };
+
+    const dailyAverageCZK = calculateAveragePrice("CZK", currentDate);
+    const dailyAverageEUR = calculateAveragePrice("EUR", currentDate);
+    // const dailyAverageCZK = db
+    //   .get("bitcoinPrices")
+    //   .filter({ currency: "CZK", createdAt: currentDate })
+    //   .map("rate")
+    //   .uniq()
+    //   .mean()
+    //   .value();
+
     console.log(dailyAverageCZK);
 
-    const dailyAverageEUR = db
-      .get("bitcoinPrices")
-      .filter({ currency: "EUR", createdAt: currentDate })
-      .map("rate")
-      .uniq()
-      .mean()
-      .value();
+    // const dailyAverageEUR = db
+    //   .get("bitcoinPrices")
+    //   .filter({ currency: "EUR", createdAt: currentDate })
+    //   .map("rate")
+    //   .uniq()
+    //   .mean()
+    //   .value();
+
     console.log(dailyAverageEUR);
+
+    // const MonthlyAverageCZK = db
+    //   .get("bitcoinPrices")
+    //   .filter({ currency: "CZK", createdAt: currentMonth })
+    //   .map("rate")
+    //   .uniq()
+    //   .mean()
+    //   .value();
+
+    // console.log(MonthlyAverageCZK);
+
+    // const MonthlyAverageEUR = db
+    //   .get("bitcoinPrices")
+    //   .filter({ currency: "EUR", createdAt: currentMonth })
+    //   .map("rate")
+    //   .uniq()
+    //   .mean()
+    //   .value();
+
+    // console.log(MonthlyAverageEUR);
+
     res.json({
       currentBitcoinPrices: {
         CZK: { code: "CZK", rate: data.czk },
         EUR: { code: "EUR", rate: data.eur },
       },
       requestedAt: requestTime,
-      dailyAverageCZK: dailyAverageCZK,
-      dailyAverageEUR: dailyAverageEUR,
+      dailyAverageCZK: dailyAverageCZK.toFixed(2),
+      dailyAverageEUR: dailyAverageEUR.toFixed(2),
     });
   } catch (error) {
     res.status(500).send("Error fetching");
